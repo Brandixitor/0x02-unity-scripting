b4a4ca85da265ba671002d816a7d0304490f5712
Brandixitor <med.salah.bourouba@gmail.com>
2021-08-03 13:11:17 +0100

    Done with task 2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,16 +6,18 @@ public class CameraController : MonoBehaviour
 {
     
     public GameObject player;
+    private Vector3 offset; 
+    public GameObject camera;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = camera.transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        camera.transform.position = player.transform.position + offset;
     }
 }
